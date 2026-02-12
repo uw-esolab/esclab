d7692a085089fdba671a8fc0c38448073bf33c2d
Mike Wagner <mjwagner2@wisc.edu>
2026-02-12 17:15:00 -0600

    adding pump type, incompressibles

--- src/esclab/components/esol_flownetwork.py
+++ src/esclab/components/esol_flownetwork.py
@@ -2,6 +2,28 @@ from esclab.simulate import *
 from eeslib import fluid_properties as fp
 from eeslib.functions import convert
 import numpy as np
+from esclab.components.esol_properties import Incompressible as Inc
+
+# 23
+# 57
+# 65
+# 162
+# 4001
+# 4006
+# 4007
+# 4008
+# 6001
+# 6003
+# 6007
+# 6011
+# 6014
+# 6016
+# 6017
+# 6019
+# 6022
+# 6027
+# 6028
+# 603
 
 # ***************************************************************************************************
 #  Friction factor (taken from Piping loss model)
@@ -44,6 +66,17 @@ def FricFactor_IC(Rough, Reynold, guess):
 
 
 class SimplePipe(Component):
+    """
+    Object: ESOL4001-SimplePipe
+    Simulation Studio Model: ESOL4001-SimplePipe
+    
+
+    Author: Matt Tuman
+    Editor: Mike Wagner
+    Date:	 September 19, 2022
+    last modified: October 13, 2022
+    Ported by: Mike Wagner, February 12, 2026
+    """
     # Create types
     Pipe_ID = Component.Parameter()         #pipe inner diameter
     Length_Pipe = Component.Parameter()     #length of pipe
@@ -104,6 +137,156 @@ class SimplePipe(Component):
             self.DELTA_P.value = 0.
             # -----------------------------------------------------------------------------------------------------------------------
         
+
+# -----------------------------------------------------------------------------------------------
+class VarSpeedPump(Component):
+    """
+    Docstring for VarSpeedPump
+    Subroutine Type4004
+    Object: ESOL4004-VarPump
+    
+    Simulation Studio Model: ESOL4004-VarPump
+    
+    Author: Matt Tuman
+    Editor: Mike Wagner
+    Date:	 January 05, 2023
+    last modified: January 05, 2023
+    Converted by: Mike Wagner, February 12, 2026
+    """
+
+    #     PARAMETERS
+    N_pumps_parallel = Component.Parameter()         #  Number of pumps that are in parallel
+    Pump_a0 = Component.Parameter()                  #  head = Pump_a0 + Pump_a1*Q_dot + Pump_a2*Q_dot^2 [-]
+    Pump_a1 = Component.Parameter()                  #  head = Pump_a0 + Pump_a1*Q_dot + Pump_a2*Q_dot^2 [1/m^3/s]
+    Pump_a2 = Component.Parameter()                  #  head = Pump_a0 + Pump_a1*Q_dot + Pump_a2*Q_dot^2 [1/m^3/s]
+    NPSH_a0 = Component.Parameter()                  #  NPSHr = Pump_a0 + Pump_a1*Q_dot + Pump_a2*Q_dot^2 [-]
+    NPSH_a1 = Component.Parameter()                  #  NPSHr = Pump_a0 + Pump_a1*Q_dot + Pump_a2*Q_dot^2 [1/m^3/s]
+    NPSH_a2 = Component.Parameter()                  #  NPSHr = Pump_a0 + Pump_a1*Q_dot + Pump_a2*Q_dot^2 [1/m^3/s]
+    RPM_full = Component.Parameter()                 #  RPM of pump at 100% speed 
+    LR = Component.Parameter()                       #  Learning rate: Close to 1 means that the mass flow can change significantly [0-1]
+    D_outlet = Component.Parameter()                 #  Diameter of the pump outlet [-]
+    Fluid_ID = Component.Parameter()                 #  Fluid ID
+    Pump_Solver = Component.Parameter()              #  Determines if hydraulic solver is used to update mass flow
+
+    #     INPUTS
+    Mass_Flow = Component.Input()                #  Mass flow into the pump [kg/s]
+    Pressure = Component.Input()                 #  Pressure of fluid at inlet of the pump [Pa]
+    Temperature = Component.Input()              #  Temperature of fluid at inlet of pump [C]
+    speed = Component.Input()                    #  Speed that the pump is operating at [0-1]
+    error = Component.Input()                    #  Error accumulated within pressure drops throughout the system [Pa]
+    mass_count = Component.Input()               #  Total mass counted before the pump [kg]
+
+    #      Outputs
+    m_dot_out = Component.Output() #  Mass Flow
+    P_out = Component.Output() #  Pressure
+    Temperature_out = Component.Output() #  Temperature
+    mass_count_out = Component.Output()
+    cavitation = Component.Output()
+    g = 9.81 #  gravity
+
+    # -----------------------------------------------------------------------------------------------------------------------
+    def setup(self, **kwargs):
+        # Set the Initial Values of the Outputs (#,Value)
+        self.m_dot_out.value = self.Mass_Flow.value
+        self.P_out.value = self.Pressure.value
+        self.Temperature_out.value = self.Temperature.value
+        self.mass_count_out.value = self.mass_count.value
+        self.cavitation.value = 0    #  Not cavitating initially
+    # -----------------------------------------------------------------------------------------------------------------------
+
+    # -----------------------------------------------------------------------------------------------------------------------
+    def converge(self):
+        # Once model has converged, check if pump is likely cavitating
+        rho = Inc.density(self.Fluid_ID.value, self.Temperature.value, 0.0)
+        Q_dot = self.Mass_Flow.value/rho/self.N_pumps_parallel.value
+        NPSHr = self.speed.value**2 * (self.NPSH_a0.value + self.NPSH_a1.value*(Q_dot/self.speed.value) + self.NPSH_a2.value*(Q_dot/self.speed.value)**2) 
+        NPSH_meas = self.Pressure.value/rho/self.g - 1034000.0/rho/self.g #  NPSH in the simulation is relative to the pressure in the expansion tank (reason for subtracting 150 psi)
+        if(NPSHr>NPSH_meas):
+            self.cavitation.value = 1.0
+        else:
+            self.cavitation.value = 0.0
+    # -----------------------------------------------------------------------------------------------------------------------
+
+      
+    def calculate(self):
+        # -----------------------------------------------------------------------------------------------------------------------
+        
+        if (self.model.iteration == 0) or (self.model.iteration == 0 and self.model.time == self.model.settings.timestep):
+            #  Set output values to the computed values from the previous timestep (don't want any manipulation without feedback)
+            #  Determine pressure increase in pump
+            rho = Inc.density(self.Fluid_ID.value, self.Temperature.value, 0.0)
+            delta_P = self.g*self.speed.value**2*(
+                rho*self.Pump_a0.value + 
+                self.Pump_a1.value/self.speed.value*(self.Mass_Flow.value/self.N_pumps_parallel.value) + 
+                self.Pump_a2.value/rho/self.speed.value**2*(self.Mass_Flow.value/self.N_pumps_parallel.value)**2
+                )
+
+            # Set the Initial Values of the Outputs (#,Value)
+            self.m_dot_out.value = self.Mass_Flow.value
+            self.P_out.value = self.Pressure.value+delta_P
+            self.Temperature_out.value = self.Temperature.value
+            self.mass_count_out.value = self.mass_count.value 
+            # self.cavitation.value #don't change
+
+        else:
+            if(self.Pump_Solver.value == 0.0):
+                #  Compute Density of fluid
+                rho = Inc.density(self.Fluid_ID.value, self.Temperature.value, 0.0)
+
+                #  Compute head loss in system
+                delta_P = self.P_out.value-(self.Pressure.value+self.error.value)
+                H_L = delta_P/rho/9.81 * 3.28084 #  [ft]
+
+                #  Compute new flow rate corresponding to head loss in system
+                #  (solving a quadratic equation for the pump curve fit)
+                A = self.Pump_a2.value
+                B = self.Pump_a1.value*self.speed.value
+                D = self.Pump_a0.value*(self.speed.value**2) - H_L
+
+                discriminant = (B)**2 - 4*A*D
+                if (discriminant>=0):
+                    sol1 = (-B + np.sqrt(discriminant))/(2*A)
+                    sol2 = (-B - np.sqrt(discriminant))/(2*A)
+                else:
+                    discriminant = 0
+                    sol1 = (-B + np.sqrt(discriminant))/(2*A)
+                    sol2 = (-B - np.sqrt(discriminant))/(2*A)
+
+                Q_dot_new = max([sol1, sol2]) * 0.00006309019640343866 #  [m^3/s]
+                m_dot_new = Q_dot_new*rho
+
+                #  Update mass flow rate according to learning rate
+                m_dot_adj = self.LR.value*m_dot_new + (1-self.LR.value)*self.m_dot_out.value/self.N_pumps_parallel.value
+
+                P_out = (self.Pressure.value+delta_P)
+                
+                # Set the Outputs from this Model (#,Value)
+                self.m_dot_out.value = m_dot_adj*self.N_pumps_parallel.value
+                self.P_out.value = P_out
+                self.Temperature_out.value = self.Temperature.value
+                self.mass_count_out.value = self.mass_count.value
+                # self.cavitation.value # don't change
+
+            else:
+                #  Determine pressure increase in pump
+                rho = Inc.density(  self.Fluid_ID.value, self.Temperature.value, 0.0)
+                delta_P = self.g*self.speed.value**2*(rho*self.Pump_a0.value + 
+                    self.Pump_a1.value/self.speed.value*(self.Mass_Flow.value/self.N_pumps_parallel.value) + 
+                    self.Pump_a2.value/rho/self.speed.value**2*(self.Mass_Flow.value/self.N_pumps_parallel.value)**2)
+
+                # Set the Outputs from this Model (#,Value)
+                self.m_dot_out.value = self.Mass_Flow.value
+                self.P_out.value = self.Pressure.value + delta_P
+                self.Temperature_out.value = self.Temperature.value
+                self.mass_count_out.value = self.mass_count.value
+                # self.cavitation.value # don't change
+    # -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
+
+
+
+
+
 
 if __name__ == "__main__":
     
---
+++ src/esclab/components/esol_properties.py
@@ -0,0 +1,185 @@
+from esclab.simulate import *
+from eeslib import fluid_properties as fp
+from eeslib.functions import convert
+import numpy as np
+
+class Incompressible:
+    """Incompressible substance property database with density, viscosity, and specific heat, where available"""
+    
+    funcmap = {
+        "Stainless_AISI316": {
+            'density': (lambda T: 8349.38 - 0.341708*T - 0.0000865128*T*T),
+            'specheat': (lambda T: 0.368455 + 0.000399548*T - 1.70558E-07*T*T),
+        },
+        "Water (liquid)": {
+            'density': (lambda T: 1000),
+            'specheat': (lambda T: 4.181),
+        },
+        "Air": {
+            'viscosity': (lambda T: max([0.0000010765 + 7.15173E-08*T - 5.03525E-11*T*T + 2.02799E-14*T*T*T, 1.e-6])),
+            'specheat': (lambda T: 1.03749 - 0.000305497*T + 7.49335E-07*T*T - 3.39363E-10*T*T*T),
+        },
+        "Salt (68 KCl, 32 MgCl2)": {
+            'density': (lambda T: 1E-10*T*T*T - 3E-07*T*T - 0.4739*T + 2384.2),
+            'viscosity': (lambda T: 0.0146*np.exp(2230./T)*0.001),
+            'specheat': (lambda T: 1.156),
+        },
+        "Salt (8 NaF, 92 NaBF4)": {
+            'density': (lambda T: 8E-09*T*T*T - 2E-05*T*T - 0.6867*T + 2438.5),
+            'viscosity': (lambda T: 0.0877*np.exp(2240./T)*0.001),
+            'specheat': (lambda T: 1.507),
+        },
+        "Salt (25 KF, 75 KBF4)": {
+            'density': (lambda T: 2E-08*T*T*T - 6E-05*T*T - 0.7701*T + 2466.1),
+            'viscosity': (lambda T: 0.0431*np.exp(3060./T)*0.001),
+            'specheat': (lambda T: 1.306),
+        },
+        "Salt (31 RbF, 69 RbBF4)": {
+            'density': (lambda T: -1E-08*T*T*T + 4E-05*T*T - 1.0836*T + 3242.6),
+            'viscosity': (lambda T: 0.0009),
+            'specheat': (lambda T: 9.127),
+        },
+        "Salt (46.5 LiF, 11.5 NaF, 42 KF)": {
+            'density': (lambda T: -2E-09*T*T*T + 1E-05*T*T - 0.7427*T + 2734.7),
+            'viscosity': (lambda T: 0.0400*np.exp(4170./T)*0.001),
+            'specheat': (lambda T: 2.010),
+        },
+        "Salt (49 LiF, 29 NaF, 29 ZrF4)": {
+            'density': (lambda T: -2E-11*T*T*T + 1E-07*T*T - 0.5172*T + 3674.3),
+            'viscosity': (lambda T: 0.0069),
+            'specheat': (lambda T: 1.239),
+        },
+        "Salt (58 KF, 42 ZrF4)": {
+            'density': (lambda T: -6E-10*T*T*T + 4E-06*T*T - 0.8931*T + 3661.3),
+            'viscosity': (lambda T: 0.0159*np.exp(3179./T)*0.001),
+            'specheat': (lambda T: 1.051),
+        },
+        "Salt (58 LiCl, 42 RbCl)": {
+            'density': (lambda T: -8E-10*T*T*T + 1E-06*T*T - 0.689*T + 2929.5),
+            'viscosity': (lambda T: 0.0861*np.exp(2517./T)*0.001),
+            'specheat': (lambda T: 8.918),
+        },
+        "Salt (58 NaCl, 42 MgCl2)": {
+            'density': (lambda T: -5E-09*T*T*T + 2E-05*T*T - 0.5298*T + 2444.1),
+            'viscosity': (lambda T: 0.0286*np.exp(1441./T)*0.001),
+            'specheat': (lambda T: 1.080),
+        },
+        "Salt (59.5 LiCl, 40.5 KCl)": {
+            'density': (lambda T: 1E-09*T*T*T - 5E-06*T*T - 0.864*T + 2112.6),
+            'viscosity': (lambda T: 0.0861*np.exp(2517./T)*0.001),
+            'specheat': (lambda T: 1.202),
+        },
+        "Salt (59.5 NaF, 40.5 ZrF4)": {
+            'density': (lambda T: -5E-09*T*T*T + 2E-05*T*T - 0.9144*T + 3837.),
+            'viscosity': (lambda T: 0.0767*np.exp(3977./T)*0.001),
+            'specheat': (lambda T: 1.172),
+        },
+        "Salt (60 NaNO3, 40 KNO3)": {
+            'density': (lambda T: max([-1E-07*T*T*T + 0.0002*T*T - 0.7875*T + 2299.4, 1000.0])),
+            'viscosity': (lambda T: max([-1.473302E-10*(T-273.15)**3 + 2.279989E-07*(T-273.15)**2 - 1.199514E-04*(T-273.15) + 2.270616E-02, 0.0001])),
+            'specheat': (lambda T: -1E-10*T*T*T + 2E-07*T*T + 5E-06*T + 1.4387),
+        },
+        "Nitrate Salt": {
+            'density': (lambda T: max([2090 - 0.636 * (T-273.15), 1000.0])),
+            'viscosity': (lambda T: max([(22.714 - 0.12 * (T-273.15) + 0.0002281 * (T-273.15)**2 - 0.0000001474 * (T-273.15)**3) / 1000, 1.e-6])),
+            'specheat': (lambda T: (1443. + 0.172 * (T-273.15))/1000.),
+        },
+        "Caloria HT 43": {
+            'density': (lambda T: max([885 - 0.6617 * (T-273.15) - 0.0001265 * (T-273.15)**2, 100.0])),
+            'specheat': (lambda T: (3.88 * (T-273.15) + 1606.0)/1000.),
+        },
+        "HITEC XL Nitrate Salt,": {
+            'density': (lambda T: max([2240 - 0.8266 * (T-273.15), 800.0])),
+            'viscosity': (lambda T: 1372000 * (T-273.15)**-3.364),
+            'specheat': (lambda T: max([1536 - 0.2624 * (T-273.15) - 0.0001139 * (T-273.15)**2, 1000.])/1000.),
+        },
+        "Therminol Oil": {
+            'density': (lambda T: max([1074.0 - 0.6367 * (T-273.15) - 0.0007762 * (T-273.15)**2, 400.0])),
+            'viscosity': (lambda T: 0.001 * (10**0.8703 * max(T-273.15, 20.)**(0.2877 + np.log10(max(T-273.15, 20.)**-0.3638)))),
+            'specheat': (lambda T: 1.509 + 0.002496 * (T-273.15) + 0.0000007888 * (T-273.15)**2),
+        },
+        "HITEC Salt": {
+            'density': (lambda T: max([2080 - 0.733 * (T-273.15), 1000.0])),
+            'viscosity': (lambda T: max([0.00622 - 0.0000102 * (T-273.15), 1.e-6])),
+            'specheat': (lambda T: (1560 - 0.0 * (T-273.15))/1000.),
+        },
+        "Dowtherm Q": {
+            'density': (lambda T: max([-0.757332 * (T-273.15) + 980.787, 100.0])),
+            'viscosity': (lambda T: 1 / (132.40658 + 4.36107 * (T-273.15) + 0.0781417 * (T-273.15)**2 - 0.00011035416 * (T-273.15)**3)),
+            'specheat': (lambda T: (-0.00053943 * (T-273.15)**2 + 3.2028 * (T-273.15) + 1589.2)/1000.),
+        },
+        "Dowtherm RP": {
+            'density': (lambda T: max([-0.000186495 * (T-273.15)**2 - 0.668337 * (T-273.15) + 1042.11, 200.0])),
+            'viscosity': (lambda T: 1 / (4.523003 + 0.39156855 * (T-273.15) + 0.028604206 * (T-273.15)**2)),
+            'specheat': (lambda T: (-0.0000031915 * (T-273.15)**2 + 2.977 * (T-273.15) + 1560.8)/1000.),
+        },
+        "HITEC XL": {
+            'density': (lambda T: max([2240 - 0.8266 * (T-273.15), 800.0])),
+            'viscosity': (lambda T: 1372000 * (T-273.15)**-3.364),
+            'specheat': (lambda T: max([1536 - 0.2624 * (T-273.15) - 0.0001139 * (T-273.15)**2, 1000.])/1000.),
+        },
+        "T-91 Steel": {
+            'density': (lambda T: -0.3289*(T-273.15) + 7742.5),
+            'specheat': (lambda T: 0.0004*(T-273.15)**2 + 0.2473*(T-273.15) + 450.08),
+        },
+        "Therminol 66": {
+            'density': (lambda T: -0.7146*(T-273.15) + 1024.8),
+            'viscosity': (lambda T: (1.31959963 - 0.171204729*(T-273.15) + 0.0100351594*(T-273.15)**2 - 0.000313556341*(T-273.15)**3 + 0.0000053430666*(T-273.15)**4 - 4.66597650E-08*(T-273.15)**5 + 1.63046296E-10*(T-273.15)**6) if (T-273.15) < 80. else (0.0490075884 - 0.00120478233*(T-273.15) + 0.0000130162082*(T-273.15)**2 - 7.58913847E-08*(T-273.15)**3 + 2.47856063E-10*(T-273.15)**4 - 4.26872345E-13*(T-273.15)**5 + 3.01949160E-16*(T-273.15)**6)),
+            'specheat': (lambda T: 0.0036*(T-273.15) + 1.4801),
+        },
+        "Therminol 59": {
+            'density': (lambda T: -0.0003*(T-273.15)**2 - 0.6963*(T-273.15) + 988.44),
+            'viscosity': (lambda T: (0.0137267822 - 0.000218740224*(T-273.15) + 0.0000759248815*(T-273.15)**2 - 0.00000473464744*(T-273.15)**3 - 1.97083667E-07*(T-273.15)**4 + 4.35487179E-09*(T-273.15)**5 + 2.40243056E-10*(T-273.15)**6) if (T-273.15) < 25. else (0.0114608807 - 0.000313431056*(T-273.15) + 0.00000416778121*(T-273.15)**2 - 3.04668508E-08*(T-273.15)**3 + 1.23719006E-10*(T-273.15)**4 - 2.60834697E-13*(T-273.15)**5 + 2.22227675E-16*(T-273.15)**6)),
+            'specheat': (lambda T: 0.0033*(T-273.15) + 1.6132),
+        },
+        "Argon": {
+            'density': (lambda T, P: max([P/(208.13*T), 1.e-10])),
+            'viscosity': (lambda T: 4.4997e-6 + 6.38920E-08*T - 1.24550E-11*T*T),
+            'specheat': (lambda T: 0.5203),
+        },
+        "Hydrogen": {
+            'density': (lambda T, P: max([P/(4124.*T), 1.e-10])),
+            'viscosity': (lambda T: 0.00000231 + 2.37842E-08*T - 5.73624E-12*T*T),
+            'specheat': (lambda T: min([max([-45.4022 + 0.690156*T - 0.00327354*T*T + 0.00000817326*T**3 - 1.13234E-08*T**4 + 8.24995E-12*T**5 - 2.46804E-15*T**6, 11.3]), 14.7])),
+        },
+        "Dowtherm A": {
+            'viscosity': (lambda T: 0.786512*max(T-273.15, 20.)**-1.44263),
+            'specheat': (lambda T: 1.47524 + 0.00368606*((T-273.15)-273) - 0.00000516458*((T-273.15)-273)**2 + 8.99399E-09*((T-273.15)-273)**3),
+        },
+    }
+
+    def density(self, fluid: str, T: float, P: float = float('nan')):
+        """Calculate density [kg/m^3] from temperature [K] and optional pressure [Pa].
+        Argon and Hydrogen require pressure."""
+        assert fluid in self.funcmap.keys(), f"Fluid '{fluid}' not found in database"
+        assert 'density' in self.funcmap[fluid], f"No density data available for '{fluid}'"
+        
+        if fluid in ['Argon', 'Hydrogen']:
+            assert P == P, "Pressure required for Argon and Hydrogen"
+            return self.funcmap[fluid]['density'](T, P)
+        else:
+            return self.funcmap[fluid]['density'](T)
+
+    def viscosity(self, fluid: str, T: float, P: float = float('nan')):
+        """Calculate dynamic viscosity [Pa-s] from temperature [K] and optional pressure [Pa]."""
+        assert fluid in self.funcmap.keys(), f"Fluid '{fluid}' not found in database"
+        assert 'viscosity' in self.funcmap[fluid], f"No viscosity data available for '{fluid}'"
+        return self.funcmap[fluid]['viscosity'](T)
+
+    def specheat(self, fluid: str, T: float, P: float = float('nan')):
+        """Calculate specific heat [kJ/kg-K] from temperature [K] and optional pressure [Pa]."""
+        assert fluid in self.funcmap.keys(), f"Fluid '{fluid}' not found in database"
+        assert 'specheat' in self.funcmap[fluid], f"No specific heat data available for '{fluid}'"
+        return self.funcmap[fluid]['specheat'](T)
+    
+    def print_fluids(self):
+        """Print list of fluids in database"""
+        for fluid in self.funcmap.keys():
+            available_properties = self.funcmap[fluid].keys()
+            abbrev_properties = {'density': 'ρ', 'viscosity': 'μ', 'specheat': 'c'}
+            abbrev_available = [abbrev_properties[prop] for prop in available_properties]
+            print(fluid + " → (" + ", ".join(abbrev_available) + ")")
+
+if __name__ == "__main__":
+    props = Incompressible()
+    props.print_fluids()
