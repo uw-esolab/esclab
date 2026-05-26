27c67196754964476fe2bf70baebcdd9784422e1
Mike Wagner <mjwagner2@wisc.edu>
2026-05-25 20:49:09 -0500

    Revised import structure

--- src/esclab/components/__init__.py
+++ src/esclab/components/__init__.py
@@ -1,13 +1,36 @@
-"""Component package exports."""
+"""
+This file provides a mechanism for importing components into models directly
+without requiring the intermediate '.components' namespace. 
 
-from .flownetwork import CV_data, FricFactor_IC, SimplePipe, TeeOut, TeeReturn, Valve, VarSpeedPump
+Add modules to this file using:
+	from . import my_component_library
+    
+And add the module name to __all__ to make it available using the flat import syntax:
+	from esclab.components import my_component_library
+    or
+    from esclab.components import *
+
+It's best practice to import the component library as a named item, and then 
+reference the components through that namespace to avoid name collisions across libraries. For example:
+	import esclab.circuit_elements as ce
+	ce.Resistor(...)
+
+Import the submodule you need and reference components through it to avoid
+name collisions across libraries:
+
+    from esclab.components import circuit_elements
+    circuit_elements.TeeOut(...)
+
+"""
+
+from . import brayton_simple
+from . import circuit_elements
+from . import esol_properties
+from . import flownetwork
 
 __all__ = [
-	"CV_data",
-	"FricFactor_IC",
-	"SimplePipe",
-	"TeeOut",
-	"TeeReturn",
-	"Valve",
-	"VarSpeedPump",
+    "brayton_simple",
+    "circuit_elements",
+    "esol_properties",
+    "flownetwork",
 ]
